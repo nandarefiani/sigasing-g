dd330d09565cc7238017795c9f2877deb2faeef1
Nanda Refiani <nandarefiani07@gmail.com>
2022-01-25 22:52:17 +0800

    Initial commit

--- index.php
+++ index.php
@@ -5,3 +5,14 @@
 
 <body class="hold-transition sidebar-mini">
   <div class="wrapper">
+    <?php include "partials/nav.php" ?>
+    <?php include "partials/sidebar.php" ?>
+    <div class="content-wrapper">
+      <?php include "routes.php" ?>
+    </div>
+    <?php include "partials/control.php" ?>
+    <?php include "partials/footer.php" ?>
+  </div>
+</body>
+
+</html>
---
+++ partials/footer.php
@@ -0,0 +1,7 @@
+  <footer class="main-footer">
+      <div class="float-right d-none d-sm-inline">
+          <b>Version</b> 0.1.0
+      </div>
+      <!-- Default to the left -->
+      <strong>Copyright &copy; 2022 <a href="#">Praktikum Web</a>.</strong> All rights reserved.
+  </footer>
